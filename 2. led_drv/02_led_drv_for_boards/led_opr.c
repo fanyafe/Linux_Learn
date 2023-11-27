c3dd2f89788b2b98b3672d14534615a12342cd1c
fanyafe <82426847+fanyafe@users.noreply.github.com>
2023-11-27 21:58:26 +0800

    Update led_opr.c

--- 2. led_drv/02_led_drv_for_boards/led_opr.c
+++ 2. led_drv/02_led_drv_for_boards/led_opr.c
@@ -2,7 +2,7 @@
 #define _LED_OPR_H
 
 struct led_operations {
-	int num;
+	int num;                 // 多少盏灯
 	int (*init) (int which); /* 初始化LED, which-哪个LED */       
 	int (*ctl) (int which, char status); /* 控制LED, which-哪个LED, status:1-亮,0-灭 */
 };
