3ba811ea1fd5dcc4c9b05aa2287add7b938c412e
fanyafe <82426847+fanyafe@users.noreply.github.com>
2023-11-28 10:21:02 +0800

    Update led_resource.h

--- 2. led_drv/03_led_drv_template_seperate/led_resource.h
+++ 2. led_drv/03_led_drv_template_seperate/led_resource.h
@@ -2,12 +2,14 @@
 #define _LED_RESOURCE_H
 
 /* GPIO3_0 */
-/* bit[31:16] = group */
-/* bit[15:0]  = which pin */
+/* bit[31:16] = group    那一组*/
+/* bit[15:0]  = which pin  哪一个引脚*/
+
 #define GROUP(x) (x>>16)
 #define PIN(x)   (x&0xFFFF)
 #define GROUP_PIN(g,p) ((g<<16) | (p))
 
+// 定义一个引脚
 struct led_resource {
 	int pin;
 };
