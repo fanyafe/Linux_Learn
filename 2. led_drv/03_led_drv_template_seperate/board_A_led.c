538f0e55a35ac1dad206b569d58a4ebbef3e1b85
fanyafe <82426847+fanyafe@users.noreply.github.com>
2023-11-28 10:24:19 +0800

    Update board_A_led.c

--- 2. led_drv/03_led_drv_template_seperate/board_A_led.c
+++ 2. led_drv/03_led_drv_template_seperate/board_A_led.c
@@ -1,12 +1,11 @@
-
 #include "led_resource.h"
-
+// 结构体 
 static struct led_resource board_A_led = {
-	.pin = GROUP_PIN(3,1),
+	.pin = GROUP_PIN(3,1),     // 第三组里面的第一个引脚
 };
 
 struct led_resource *get_led_resouce(void)
 {
-	return &board_A_led;
+	return &board_A_led;    // 返回
 }
 
